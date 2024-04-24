7478728f87a2f3dd90e63a639eb7aaa9e5671a90
shivani sharma <shikha@shivanis-MacBook-Air.local>
2024-04-23 22:43:05 -0400

    added jobPostingEdit

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
 import React from "react";
+import logo from "./logo.svg";
 import "./App.css";
+import { decryptResponse } from "./utils/decryptorUtil";
+import { getTopCompanies } from "./externalApis/levelsFyiClient";
 import { BrowserRouter, Route, Routes, useLocation, useNavigate } from "react-router-dom";
 import ForgotPassword from "./users/components/ForgotPassword";
 import SeekerDetailPage from "./public/pages/SeekerDetailPage";
@@ -12,8 +15,11 @@ import Layout from "./layouts/ParentLayout";
 import CreateRecruiter from "./recruiters/CreateRecruiter";
 import CreateSeeker from "./seekers/CreateSeeker";
 import SeekJobs from "./seekers/SeekJobs";
+
 import CreateJobPosting from "./jobPostings/jobPosting";
+
 import ReviewApplications from "./recruiters/ReviewApplications";
+import { getMyUser } from "./users/client";
 import { AuthContext, AuthProvider } from "./AuthContext";
 import SignUpConfirmPage from "./users/pages/SignUpConfirmPage";
 import RecruiterEdit from "./public/components/RecruiterEdit";
@@ -22,11 +28,27 @@ import AdminSignInPage from "./admin/pages/AdminSignInPage";
 import AdminDashboardPage from "./admin/pages/AdminDashboardPage";
 import RecruiterDashboard from "./recruiters/RecruiterDashboard";
 import SeekerDashboard from "./seekers/SeekerDashboard";
-import ApplyJobs from "./seekers/ApplyJobs";
-import ShortlistCandidates from "./recruiters/ShortlistCandidates";
+import JobPostingsEdit from "./jobPostings/jobPostingsEdit";
 
 function App() {
   let [user, setUser] = React.useState({});
+
+  const test = () => {
+    let payload =
+      "PGF/4hzKuy89+4RiRL0I/FNOde3SCoZvSJHnCnvdPtOUbWkCh9LrNVXSDDUkchmE8HR9MUYeGpTAv9LCp6fVDjdQXuezZQk2bR4yWFP9PktIuKqngYCnSibUlz1ZzwlAeW3GWyFWLHRYKR5r5qMjqIQnT2Z3xL53sq9crdRlrH5yR2Kk05xid8/fdQvMQFKUnc81BZD8z70nRBfMplORE7nBrArCKiNPXN43E6+GVzM=";
+
+    console.log(decryptResponse({ payload: payload }));
+  };
+
+  const testApiCall = async () => {
+    const res = await getTopCompanies();
+    console.log(res);
+  };
+
+  const getCurrUser = async () => {
+    let user = await getMyUser();
+    return user;
+  };
 
   const init = async () => {
     if (window.location.pathname.startsWith("/logout")) user = {};
@@ -36,7 +58,7 @@ function App() {
 
   React.useEffect(() => {
     init();
-  });
+  }, []);
 
   return (
     <div className="">
@@ -57,17 +79,18 @@ function App() {
               <Route path="/seekers/:username" element={<SeekerDetailPage />} />
               <Route path="/recruiters/:username" element={<RecruiterDetailPage />} />
               <Route path="/job-posting" element={<CreateJobPosting />} />
+              <Route path="/job-posting/:jobPostingId" element={<JobPostingsEdit />} />
               <Route path="/forgot-password" element={<ForgotPassword />} />
               <Route path="/create-recruiter" element={<CreateRecruiter />} />
               <Route path="/create-seeker" element={<CreateSeeker />} />
+              <Route path="/seek-jobs" element={<SeekJobs />} />
               <Route path="/recruiters/edit" element={<RecruiterEdit />} />
               <Route path="/seekers/edit" element={<SeekerEdit />} />
               <Route path="/seek-jobs" element={<SeekJobs />} />
+              <Route path="/review-applications" element={<ReviewApplications />} />
               <Route path="/recruiters/dashboard" element={<RecruiterDashboard />} />
               <Route path="/seekers/dashboard" element={<SeekerDashboard />} />
-              <Route path="/review-applications" element={<ReviewApplications />} />
-              <Route path="/apply-jobs" element={<ApplyJobs />} />
-              <Route path="/shortlist-candidates" element={<ShortlistCandidates />} />
+
             </Routes>
           </BrowserRouter>
         </Layout>
